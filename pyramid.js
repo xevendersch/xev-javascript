9c370acb21f41679fca74997e2ed8dcc54a7b28d
xevendersch <muhammad.alqaaf_subandoko.mb5@is.naist.jp>
2024-04-24 22:14:03 +0900

    Added first javascript file

---
+++ pyramid.js
@@ -0,0 +1,18 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
+    rows.push(padRow(i, count));
+}
+
+let results = "";
+
+for (const row of rows) {
+    results = results + "\n" + row;
+}
+console.log(results);
