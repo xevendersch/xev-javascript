25c3135f921415dfc00ef37c9ae62b260290230c
xevendersch <muhammad.alqaaf_subandoko.mb5@is.naist.jp>
2024-04-25 00:41:59 +0900

    Updated pyramid.js

--- pyramid.js
+++ pyramid.js
@@ -6,7 +6,7 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 1; i < count; i++) {
+for (let i = 1; i <= count; i++) {
     rows.push(padRow(i, count));
 }
 
